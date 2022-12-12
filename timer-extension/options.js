24d9623305fa847e49781ec3171468a9693307f1
Anjan Talatam <tspa2117@gmail.com>
2022-12-12 22:49:45 +0530

    10- Chrome storage API

--- timer-extension/options.js
+++ timer-extension/options.js
@@ -2,5 +2,16 @@ const nameInput = document.getElementById("name-input");
 const saveBtn = document.getElementById("save-btn");
 
 saveBtn.addEventListener("click", () => {
-  console.log(nameInput.value);
+  const name = nameInput.value;
+
+  // set user name in chrome storage when save button is clicked
+  chrome.storage.sync.set({ name }, () => {
+    console.log(`Name has been set to ${name}`);
+  });
+});
+
+// retrieve saved user name on load
+chrome.storage.sync.get(["name", "test"], (res) => {
+  const name = res.name ?? "???";
+  nameInput.value = name;
 });
--- timer-extension/popup.js
+++ timer-extension/popup.js
@@ -1,27 +1,10 @@
 const timeElement = document.getElementById("time");
+const nameElement = document.getElementById("name");
 const now = new Date().toLocaleTimeString();
 
 timeElement.textContent = `The time is: ${now}`;
 
-// // method to set title from code
-// chrome.action.setTitle({
-//   title: "Popup opened",
-// });
-
-// // badge: Refer Focus Todo app that shows timer as the badge
-// chrome.action.setBadgeText(
-//   {
-//     text: "25",
-//   },
-//   () => {
-//     setTimeout(() => {
-//       chrome.action.setBadgeText({ text: "2525" });
-//     }, 2000);
-//   }
-// );
-
-// // set background color for badge
-// chrome.action.setBadgeBackgroundColor({ color: "#00FF00" });
-
-// // set badge text color
-// // chrome.action.setBadgeTextColor({ color: "pink" }); // not working
+chrome.storage.sync.get(["name"], (res) => {
+  const name = res.name ?? "???";
+  nameElement.innerText = `Your name is ${name}`;
+});

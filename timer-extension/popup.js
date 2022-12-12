99d6c8632e7e6ce6e84e8f0c75108092297f77d9
Anjan Talatam <tspa2117@gmail.com>
2022-12-12 12:14:03 +0530

    9-Options Page

--- timer-extension/popup.js
+++ timer-extension/popup.js
@@ -3,25 +3,25 @@ const now = new Date().toLocaleTimeString();
 
 timeElement.textContent = `The time is: ${now}`;
 
-// method to set title from code
-chrome.action.setTitle({
-  title: "Popup opened",
-});
+// // method to set title from code
+// chrome.action.setTitle({
+//   title: "Popup opened",
+// });
 
-// badge: Refer Focus Todo app that shows timer as the badge
-chrome.action.setBadgeText(
-  {
-    text: "25",
-  },
-  () => {
-    setTimeout(() => {
-      chrome.action.setBadgeText({ text: "2525" });
-    }, 2000);
-  }
-);
+// // badge: Refer Focus Todo app that shows timer as the badge
+// chrome.action.setBadgeText(
+//   {
+//     text: "25",
+//   },
+//   () => {
+//     setTimeout(() => {
+//       chrome.action.setBadgeText({ text: "2525" });
+//     }, 2000);
+//   }
+// );
 
-// set background color for badge
-chrome.action.setBadgeBackgroundColor({ color: "#00FF00" });
+// // set background color for badge
+// chrome.action.setBadgeBackgroundColor({ color: "#00FF00" });
 
-// set badge text color
-// chrome.action.setBadgeTextColor({ color: "pink" }); // not working
+// // set badge text color
+// // chrome.action.setBadgeTextColor({ color: "pink" }); // not working

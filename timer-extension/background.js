6545a127d37fd044393bcfd209e3c33e6c3f8c5e
Anjan Talatam <tspa2117@gmail.com>
2022-12-13 22:49:40 +0530

    12- Chrome alarms API

--- timer-extension/background.js
+++ timer-extension/background.js
@@ -1,10 +1,20 @@
-console.log("Hello from Service worker");
+// create a chrome alarm which runs every one second
+// => every 1 second alarm event is fired
+chrome.alarms.create({
+  periodInMinutes: 1 / 60,
+});
 
-console.log(this); //ServiceWorkerGlobalScope
+chrome.alarms.onAlarm.addListener((alarm) => {
+  // every 1 second this will be executed
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = res.timer ?? 0;
 
-let time = 0;
+    chrome.storage.local.set({
+      timer: time + 1,
+    });
 
-setInterval(() => {
-  time += 1;
-  console.log(time);
-}, 1000);
+    chrome.action.setBadgeText({
+      text: `${time + 1}`,
+    });
+  });
+});
--- timer-extension/popup.js
+++ timer-extension/popup.js
@@ -1,10 +1,23 @@
 const timeElement = document.getElementById("time");
 const nameElement = document.getElementById("name");
-const now = new Date().toLocaleTimeString();
-
-timeElement.textContent = `The time is: ${now}`;
+const timerElement = document.getElementById("timer");
 
 chrome.storage.sync.get(["name"], (res) => {
   const name = res.name ?? "???";
   nameElement.innerText = `Your name is ${name}`;
 });
+
+function updateTimer() {
+  const now = new Date().toLocaleTimeString();
+  timeElement.textContent = `The time is: ${now}`;
+
+  chrome.storage.local.get(["timer"], (res) => {
+    const timer = res.timer ?? 0;
+    timerElement.innerText = `The timer is at: ${timer} seconds`;
+  });
+}
+
+updateTimer();
+setInterval(() => {
+  updateTimer();
+}, 1000);
